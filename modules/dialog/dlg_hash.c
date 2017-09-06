ad20d0ba9f5f846e074f13a1dc54849b616ffd4a
Royee Tichauer <Royee.Tichauer@vonage.com>
2017-09-06 09:38:11 +0300

    no message

--- modules/dialog/dlg_hash.c
+++ modules/dialog/dlg_hash.c
@@ -329,6 +329,7 @@ struct dlg_cell* build_new_dlg( str *callid, str *from_uri, str *to_uri,
 int dlg_update_leg_info(struct dlg_leg *leg, struct dlg_cell *dlg, str *tag, str *rr,
                         str *contact, str *cseq, struct socket_info *sock,
                         str *mangled_from, str *mangled_to) {
+    rr_t *head = NULL, *rrp;
 
     if (contact->len) {
         /* contact */
@@ -337,11 +338,37 @@ int dlg_update_leg_info(struct dlg_leg *leg, struct dlg_cell *dlg, str *tag, str
         }
         leg->contact.s = shm_malloc(rr->len + contact->len);
         if (leg->contact.s == NULL) {
+            LM_ERR("no more shm mem\n");
+            shm_free(leg->tag.s);
+            shm_free(leg->r_cseq.s);
             return -1;
         }
         leg->contact.len = contact->len;
         memcpy(leg->contact.s, contact->s, contact->len);
+        /* rr */
+        if (rr->len) {
+            leg->route_set.s = leg->contact.s + contact->len;
+            leg->route_set.len = rr->len;
+            memcpy(leg->route_set.s, rr->s, rr->len);
+            LM_ERR("Royee route_set=%.*s\n", leg->route_set.s, leg->route_set.len);
+            if (parse_rr_body(leg->route_set.s, leg->route_set.len, &head) != 0) {
+                LM_ERR("failed parsing route set\n");
+                shm_free(leg->contact.s);
+                return -1;
+            }
+            rrp = head;
+            leg->nr_uris = 0;
+            while (rrp) {
+                leg->route_uris[leg->nr_uris++] = rrp->nameaddr.uri;
+                LM_ERR("Royee rrp->nameaddr.uri.s=%.*s\n", rrp->nameaddr.uri.s, rrp->nameaddr.uri.len);
+                rrp = rrp->next;
+            }
+            free_rr(&head);
+        }
     }
+
+    /* socket */
+    leg->bind_addr = sock;
 
     return 0;
 }
